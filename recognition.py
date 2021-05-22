3f394242fd2bf5ec3dac82dcfceed5ae8047218b
Evan Nguyen <evannguyen1101@yahoo.com>
2021-05-22 01:22:58 -0700

    first deployment
    opted for a hashed dictionary instead of a tree. worse in terms of flexibility but definitely works this time.
    
    Currently only responds to 'hello'/'hi'

--- recognition.py
+++ recognition.py
@@ -24,15 +24,39 @@ import nltk
 #nltk.download('averaged_perceptron_tagger')
 
 class Node:
-    def __init__(self):
-        aa = 1
+    def __init__(self, name=None, param=None):
+        self.name = name
+        self.command = param
+
 
 #--------------------------------------------------------------build graph
-        
+tree = {}
+allowed = []
+
+#defined to add a single path from root to end
+def addPath(tree_dict, connections, command_str):
+    h = hash(tuple(connections))
+    for word in connections:
+        if word not in allowed:
+            allowed.append(word)
+    tree[h] = command_str
+
+'''def generateList(string):
+    lst = string.split("-")
+    o = []
+    for v in lst:
+        o.append(Node(v))
+
+    return o'''
+
+def generateList(string):
+    return string.split("-")
+
+addPath(tree, generateList("athena-hello"), "response_hello(ctx)")
+addPath(tree, generateList("hi-athena"), "response_hello(ctx)")
+addPath(tree, generateList("hello-athena"), "response_hello(ctx)")
 
 #------------------------------------------------------------------
-
-
 stop_words = set(stopwords.words('english'))
 ps = PorterStemmer()
 
@@ -48,6 +72,35 @@ class NLP(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.desc = 'NLP test, designed for basic commands. Responds to its nickname.'
+        self.tree = tree
+        self.allowed = allowed
+
+    async def get_command(self, tokens):
+        uniques = []
+        valid = []
+        
+        for word, identifier in tokens:
+            if identifier == "NN" and word not in self.allowed:
+                uniques.append(word)
+            if word in self.allowed:
+                valid.append(word)
+
+        '''if out[0] == '-':
+            out = out[1:]
+        if out[-1] == '-':
+            out = out[:-1]'''
+
+        h = hash(tuple(valid))
+        cmd = None
+        try:
+            cmd = self.tree[h]
+        except:
+            pass
+        
+        return cmd
+        
+    async def response_hello(self, ctx, message=""):
+        await ctx.send("hello!")
 
     @commands.Cog.listener()
     async def on_message(self, message):
@@ -58,8 +111,7 @@ class NLP(commands.Cog):
                 ctx = await self.bot.get_context(message)
                 tk = tokenize(message.content)
 
-                await message.channel.send(tk)
+                c = await self.get_command(tk)
 
-    async def response_hello(self, ctx, message=""):
-        await ctx.send("hello!")
-
+                f = "self." + c
+                await eval(f)
